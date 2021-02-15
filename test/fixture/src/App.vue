36647b77091c61d5f80636f07e1cc1766184e047
INECO\jose.manchenos <jose.manchenos@ineco.com>
2021-02-15 02:11:23 +0100

    Eliminamos duplicados de los eventos

--- test/fixture/src/App.vue
+++ test/fixture/src/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png">
-
+<another-component-name :name="" :names=""></another-component-name>
     </div>
 </template>
 
 <script>
+import another-component-name from '@/components/AnotherComponent.vue'
 export default {
+    components: {
+        'another-component-name': AnotherComponentName,
+    },
     name: 'App',
 };
 </script>
